ac359038310c1837f69753d3a0f32c87df5f66f9
Ali Belmaaza <ali.belmaaza1@gmail.com>
2026-08-10 01:30:00 +0100

    clarifying spacing and lessons

--- Lesson1.js
+++ Lesson1.js
@@ -1,7 +1,10 @@
 //1.hello.World
+console.log("First Lesson: console.log")
 console.log("Hello World!");
+console.log(" ")
 
 //2. Variables
+console.log("Second Lesson: Variables")
 var Firstname = "Ali"
 var Lastname = "Belmaaza"
 
@@ -15,6 +18,13 @@ var age = "29"
 var sex = "Male"
 var CurrentJob = "Quality & Training Specialist - Customer Service"
 var Description = age + "\n" + sex + "\n" + CurrentJob
-var Description = age + " " + sex + " " + CurrentJob
+var Description2 = age + " " + sex + " " + CurrentJob
 console.log (Description)
+console.log (Description2)
+console.log(" ")
 
+
+//3. constants
+console.log("Third Lesson: Constants")
+const occupation = "Quality & Training Specialist - Customer Service"
+console.log (occupation)
