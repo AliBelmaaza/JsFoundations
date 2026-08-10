b0eda3cc13408233323365177a2c45ac4a1625c4
Ali Belmaaza <ali.belmaaza1@gmail.com>
2026-08-10 23:34:29 +0100

    Playing with Concatentions

--- Lesson2.js
+++ Lesson2.js
@@ -5,3 +5,18 @@ var age = 29
 var occupation = "Quality & Training Specialist - Customer Service"
 console.log("Concatenation: " + Firstname + " " + Lastname + " is " + age + " years old and works as a " + occupation + ".")
 
+
+//When I like to chill, I play Gnawa Music, such as "https://www.youtube.com/watch?v=lWK6Nt5tpOU"
+
+console.log("When I like to chill, I play Gnawa Music, such as \"https://www.youtube.com/watch?v=lWK6Nt5tpOU\".")
+
+var Mood = "chill"
+var Music = "Gnawa Music"
+var MusicLink = "https://www.youtube.com/watch?v=lWK6Nt5tpOU"
+console.log("When I like to " + Mood + ", " + "I play " + Music + ", " + "such as " + "\"" + MusicLink + "\".")
+
+var Mood = "feel blue"
+var Artist = "Gary Moore"
+var Music = "Still Got The Blues"
+var MusicLink = "https://www.youtube.com/watch?v=4O_YMLDvvnw"
+console.log("When I like to " + Mood + ", " + "I play " + Artist + "," + Music + ": " + "\"" + MusicLink + "\".")
