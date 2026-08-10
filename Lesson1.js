4d2c7700dc75a98438de0885c5562074461ea9ec
Ali Belmaaza <ali.belmaaza1@gmail.com>
2026-08-10 01:59:09 +0100

    testing boundries

--- Lesson1.js
+++ Lesson1.js
@@ -1,26 +1,43 @@
 //1.hello.World
 console.log("First Lesson: console.log")
 console.log("Hello World!");
+
+
 console.log(" ")
+
 
 //2. Variables
 console.log("Second Lesson: Variables")
 var Firstname = "Ali"
 var Lastname = "Belmaaza"
 
-console.log (Firstname)
-console.log (Lastname)
+console.log ("First Name:" +Firstname)
+console.log ("Last Name:" +Lastname)
 
 var Fullname = Firstname + Lastname
-console.log (Fullname)
-console.log (Firstname + " " + Lastname)
+console.log ("Fullname:" + Fullname)
+console.log ("Fullname: " + Firstname + " " + Lastname)
+
 var age = "29"
 var sex = "Male"
 var CurrentJob = "Quality & Training Specialist - Customer Service"
-var Description = age + "\n" + sex + "\n" + CurrentJob
-var Description2 = age + " " + sex + " " + CurrentJob
+
+var Description = "-" + age + "\n" + "-" + sex + "\n" + "-" + CurrentJob
+console.log ("Description:" + "\n" + Description)
+var Description =  +age + " ," + sex + " ," + CurrentJob
 console.log (Description)
-console.log (Description2)
+
+
+console.log(" ")
+
+
+var number1, number2, number3
+number1 = "10"
+number2 = "20"
+number3 = number1 + number2
+console.log (number3)
+number3 = number1 - number2 
+console.log (number3)
 console.log(" ")
 
 
