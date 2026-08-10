55a3e28785358f08b7205f098f8fb545ab8479f2
Ali Belmaaza <ali.belmaaza1@gmail.com>
2026-08-10 01:12:41 +0100

    Variables

--- Lesson1.js
+++ Lesson1.js
@@ -1,3 +1,20 @@
 //1.hello.World
 console.log("Hello World!");
- 
+
+//2. Variables
+var Firstname = "Ali"
+var Lastname = "Belmaaza"
+
+console.log (Firstname)
+console.log (Lastname)
+
+var Fullname = Firstname + Lastname
+console.log (Fullname)
+console.log (Firstname + " " + Lastname)
+var age = "29"
+var sex = "Male"
+var CurrentJob = "Quality & Training Specialist - Customer Service"
+var Description = age + "\n" + sex + "\n" + CurrentJob
+var Description = age + " " + sex + " " + CurrentJob
+console.log (Description)
+
