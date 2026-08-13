8b9fd2dac34ecd9eeb97c38c3a779d46e4d21f9b
Ali Belmaaza <ali.belmaaza1@gmail.com>
2026-08-13 01:34:19 +0100

    Lesson 3 Basics

---
+++ Lesson3.js
@@ -0,0 +1,15 @@
+//Objects
+
+var customer = {
+     Fistname: 'Ali',
+     LastName: 'Belmaaza',
+}
+//Dot Notation
+customer.Fistname = "Abdellah"
+//Bracket Notation
+customer['LastName'] = "Driss"
+console.log(`${customer.Fistname} ${customer.LastName}`)
+
+//Arrays
+var Bike =["Voge", "Honda", "Yamaha"]
+console.log (Bike[0],Bike[1])
